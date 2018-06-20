c7f789886e23597ffe895785bfa2c0f194e9e7cd
ist188123 <paulorosario@sapo.pt>
2018-06-20 13:57:26 +0100

    Create as.js

---
+++ as.js
@@ -0,0 +1,24 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === 'ola') {
+    msg.reply('ola, quere marcar uma RAID?');
+  }
+});
+
+
+
+client.on('message', msg => {
+  if (msg.content === 'quero') {
+    msg.reply('Qual o tipo de RAID?');
+  }
+});
+
+
+
+client.login(process.env.BOT_TOKEN);

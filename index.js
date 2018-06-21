d4b5c092312fc16d2835768794c1594cefb56af9
ist188123 <paulorosario@sapo.pt>
2018-06-21 21:23:52 +0100

    Update index.js

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ if (msg.channel.name == 'raids-marcacao') {
   
   const embed = new Discord.RichEmbed()
   .setTitle("This is your title, it can hold 256 characters")
-  .setAuthor("Author Name", "https://exraidspinhalnovo.webnode.pt/_files/200000018-6874a696da/450/regice.png")
+  .setAuthor(text, "https://exraidspinhalnovo.webnode.pt/_files/200000019-4d5f84e5ec/200/Egg_Raid_Legendary.png")
   /*
    * Alternatively, use "#00AE86", [0, 174, 134] or an integer number.
    */
@@ -40,7 +40,7 @@ if (msg.channel.name == 'raids-marcacao') {
   .setDescription("This is the main body of text, it can hold 2048 characters.")
   .setFooter("This is the footer text, it can hold 2048 characters", "http://i.imgur.com/w1vhFSR.png")
   .setImage("http://i.imgur.com/yVpymuV.png")
-  .setThumbnail("http://i.imgur.com/p2qNFag.png")
+  .setThumbnail("https://exraidspinhalnovo.webnode.pt/_files/200000018-6874a696da/450/regice.png")
   /*
    * Takes a Date object, defaults to current date.
    */

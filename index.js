0d24c4738121a7988d3d7c17d98ece747f0134cf
ist188123 <paulorosario@sapo.pt>
2018-06-21 21:34:42 +0100

    Update index.js

--- index.js
+++ index.js
@@ -23,15 +23,15 @@ if (msg.channel.name == 'raids-marcacao') {
      var text = msg.content.substring(1);
  
 	  
-   text='**RAID '+text+'**'	  
+   text='RAID '+text	  
 	  
     
       // msg.reply(text);
-	msg.guild.channels.find("name", "raids-pinhal-novo").sendMessage(text);
+	
 	//msg.channel.fetchMessage();
   
   const embed = new Discord.RichEmbed()
-  .setTitle("This is your title, it can hold 256 characters")
+  .setTitle("Piscinas")
   .setAuthor(text, "https://exraidspinhalnovo.webnode.pt/_files/200000019-4d5f84e5ec/200/Egg_Raid_Legendary.png")
   /*
    * Alternatively, use "#00AE86", [0, 174, 134] or an integer number.
@@ -39,13 +39,13 @@ if (msg.channel.name == 'raids-marcacao') {
   .setColor(0x00AE86)
   .setDescription("This is the main body of text, it can hold 2048 characters.")
   .setFooter("This is the footer text, it can hold 2048 characters", "http://i.imgur.com/w1vhFSR.png")
-  .setImage("http://i.imgur.com/yVpymuV.png")
+ // .setImage("http://i.imgur.com/yVpymuV.png")
   .setThumbnail("https://exraidspinhalnovo.webnode.pt/_files/200000018-6874a696da/450/regice.png")
   /*
    * Takes a Date object, defaults to current date.
    */
   .setTimestamp()
-  .setURL("https://discord.js.org/#/docs/main/indev/class/RichEmbed")
+  .setURL("https://www.google.com/maps/place/Pinhal+Novo/@38.6321418,-8.9198928,14.66z/data=!4m5!3m4!1s0xd19409858c24d63:0x500ebbde4910920!8m2!3d38.6318721!4d-8.9150933")
   .addField("This is a field title, it can hold 256 characters",
     "This is a field value, it can hold 1024 characters.")
   /*
@@ -59,7 +59,7 @@ if (msg.channel.name == 'raids-marcacao') {
   .addField("Inline Field 3", "You can have a maximum of 25 fields.", true);
 
   msg.channel.send({embed});
-	  
+  msg.guild.channels.find("name", "raids-pinhal-novo").sendMessage({embed});	  
 	  
 	  
 	  

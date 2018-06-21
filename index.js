85ba9b4b7722f51885513c53ddce2fc4bfe41e1c
ist188123 <paulorosario@sapo.pt>
2018-06-21 21:12:31 +0100

    Update index.js

--- index.js
+++ index.js
@@ -30,20 +30,15 @@ if (msg.channel.name == 'raids-marcacao') {
 	msg.guild.channels.find("name", "raids-pinhal-novo").sendMessage(text);
 	//msg.channel.fetchMessage();
   
-  
-	  
-	  
-	  
-	 const embed = new Discord.RichEmbed()  
-	 
+  const embed = new Discord.RichEmbed()
   .setTitle("This is your title, it can hold 256 characters")
-  .setAuthor("Author Name", "http://i.imgur.com/yVpymuV.png")
+  .setAuthor("Author Name", "https://i.imgur.com/lm8s41J.png")
   /*
    * Alternatively, use "#00AE86", [0, 174, 134] or an integer number.
    */
   .setColor(0x00AE86)
   .setDescription("This is the main body of text, it can hold 2048 characters.")
-  .setFooter("This is the footer text, it can hold 2048 characters", ":")
+  .setFooter("This is the footer text, it can hold 2048 characters", "http://i.imgur.com/w1vhFSR.png")
   .setImage("http://i.imgur.com/yVpymuV.png")
   .setThumbnail("http://i.imgur.com/p2qNFag.png")
   /*
@@ -64,7 +59,6 @@ if (msg.channel.name == 'raids-marcacao') {
   .addField("Inline Field 3", "You can have a maximum of 25 fields.", true);
 
   msg.channel.send({embed});
-	  
 	  
 	  
 	  

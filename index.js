fc4d804d1b5e9532480a2fc944f33a43ce98205d
ist188123 <paulorosario@sapo.pt>
2018-06-21 20:42:36 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-
+ const embed = new Discord.RichEmbed()
+ 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
@@ -20,10 +21,9 @@ if (msg.channel.name == 'raids-marcacao') {
 	  
   //LE A MENSAGEM EXCLUINDO O !
      var text = msg.content.substring(1);
- 
+ var local="piscinas"
 	  
    text='**RAID '+text+'**'	  
-	  
 	  
     
       // msg.reply(text);
@@ -35,15 +35,15 @@ if (msg.channel.name == 'raids-marcacao') {
 	  
 	  
 	  
-	  const embed = new Discord.RichEmbed()
+	 
   .setTitle("This is your title, it can hold 256 characters")
-  .setAuthor("Author Name", "https://i.imgur.com/lm8s41J.png")
+  .setAuthor("Author Name", "https://"+local)
   /*
    * Alternatively, use "#00AE86", [0, 174, 134] or an integer number.
    */
   .setColor(0x00AE86)
   .setDescription("This is the main body of text, it can hold 2048 characters.")
-  .setFooter("This is the footer text, it can hold 2048 characters", "http://i.imgur.com/w1vhFSR.png")
+  .setFooter("This is the footer text, it can hold 2048 characters", ":")
   .setImage("http://i.imgur.com/yVpymuV.png")
   .setThumbnail("http://i.imgur.com/p2qNFag.png")
   /*

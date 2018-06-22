358dc1216b65ed24e3cc8d761a77f14a781b52d4
ist188123 <paulorosario@sapo.pt>
2018-06-22 09:41:56 +0100

    Update index.js

--- index.js
+++ index.js
@@ -27,16 +27,16 @@ if (msg.channel.name == 'raids-marcacao') {
 	  
     //inicio mensagem
    const embed = new Discord.RichEmbed()
-  .setTitle("This is your title, it can hold 256 characters")
-  .setAuthor("Author Name", "https://i.imgur.com/lm8s41J.png")
+  .setTitle("12h30")
+  .setAuthor(text, "https://exraidspinhalnovo.webnode.pt/_files/200000019-4d5f84e5ec/200/Egg_Raid_Legendary.png")
   /*
    * Alternatively, use "#00AE86", [0, 174, 134] or an integer number.
    */
   .setColor(0x00AE86)
-  .setDescription("This is the main body of text, it can hold 2048 characters.")
-  .setFooter("This is the footer text, it can hold 2048 characters", "http://i.imgur.com/w1vhFSR.png")
-  .setImage("http://i.imgur.com/yVpymuV.png")
-  .setThumbnail("http://i.imgur.com/p2qNFag.png")
+  .setDescription("")
+  .setFooter("Fim mensagem RAID pinhal novo.", "http://i.imgur.com/w1vhFSR.png")
+ // .setImage("http://i.imgur.com/yVpymuV.png")
+  .setThumbnail("https://exraidspinhalnovo.webnode.pt/_files/200000018-6874a696da/450/regice.png")
   /*
    * Takes a Date object, defaults to current date.
    */

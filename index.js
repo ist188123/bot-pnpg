025f582bd0cdca09c276dbe133d26575f9fa12c0
ist188123 <paulorosario@sapo.pt>
2018-06-22 21:24:19 +0100

    Update index.js

--- index.js
+++ index.js
@@ -49,6 +49,7 @@ if (msg.channel.name == 'raids-marcacao') {
 	  
     //inicio mensagem
    const embed = new Discord.RichEmbed()
+   .setTile("Para se inscrever acede ao canal #sdfsdffsf")
   .setTitle(text.substr(text.indexOf('!')+1))
   .setAuthor(text.substr(0,text.indexOf('!')-1), "https://exraidspinhalnovo.webnode.pt/_files/200000019-4d5f84e5ec/200/Egg_Raid_Legendary.png")
   /*

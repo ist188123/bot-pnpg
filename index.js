96247512efd82e59a309b1f9077ee453c26732b3
ist188123 <paulorosario@sapo.pt>
2018-06-22 17:16:36 +0100

    Update index.js

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ if (msg.channel.name == 'raids-marcacao') {
 	//cria canal
 		  if(msg.guild.channels.find("name", "xpto")){
 		  }else{
-			  var canal=text.substr(text.indexOf('!')+1)+text.substr(0,text.indexOf('!')-1)
+			  var canal=text.substr(text.indexOf('!')+1)
 	msg.guild.createChannel(canal, "text");
 		  }
 		  

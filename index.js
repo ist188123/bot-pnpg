160ebffe9106318e30322a4ca9b465c23dc5e521
ist188123 <paulorosario@sapo.pt>
2018-06-22 20:17:44 +0100

    Update index.js

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ if (msg.channel.name == 'raids-marcacao') {
 		  }	  
 		  
 		  
-		 msg.guild.channels.find("name", canal).sendMessage("sss");	 
+		 msg.guild.channels.find("name", "raids-pinhal-novo").sendMessage(canal);	 
 	        
 		 
 		  

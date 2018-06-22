0b6274f13385babbd4141f16e38045187b8e336c
ist188123 <paulorosario@sapo.pt>
2018-06-22 09:39:51 +0100

    Update index.js

--- index.js
+++ index.js
@@ -26,37 +26,34 @@ if (msg.channel.name == 'raids-marcacao') {
    text='RAID '+text	  
 	  
     //inicio mensagem
-        
-const embed = new Discord.RichEmbed()
-  .setTitle("12h30")
-  .setAuthor(text, "https://exraidspinhalnovo.webnode.pt/_files/200000019-4d5f84e5ec/200/Egg_Raid_Legendary.png")
+   const embed = new Discord.RichEmbed()
+  .setTitle("This is your title, it can hold 256 characters")
+  .setAuthor("Author Name", "https://i.imgur.com/lm8s41J.png")
   /*
    * Alternatively, use "#00AE86", [0, 174, 134] or an integer number.
    */
   .setColor(0x00AE86)
-  .setDescription("")
-  .setFooter("Fim mensagem RAID pinhal novo.", "http://i.imgur.com/w1vhFSR.png")
- .setImage("http://i.imgur.com/yVpymuV.png")
-  .setThumbnail("https://exraidspinhalnovo.webnode.pt/_files/200000018-6874a696da/450/regice.png")
+  .setDescription("This is the main body of text, it can hold 2048 characters.")
+  .setFooter("This is the footer text, it can hold 2048 characters", "http://i.imgur.com/w1vhFSR.png")
+  .setImage("http://i.imgur.com/yVpymuV.png")
+  .setThumbnail("http://i.imgur.com/p2qNFag.png")
   /*
    * Takes a Date object, defaults to current date.
    */
   .setTimestamp()
-  .setURL("https://www.google.com/maps/place/Pinhal+Novo/@38.6321418,-8.9198928,14.66z/data=!4m5!3m4!1s0xd19409858c24d63:0x500ebbde4910920!8m2!3d38.6318721!4d-8.9150933")
-  .addField("TREINADORES PRESENTES", "Damasc010\nSDamasc022")
+  .setURL("https://discord.js.org/#/docs/main/indev/class/RichEmbed")
+  .addField("This is a field title, it can hold 256 characters",
+    "This is a field value, it can hold 1024 characters.")
   /*
    * Inline fields may not display as inline if the thumbnail and/or image is too big.
    */
-  .addField("", "", true)
+  .addField("Inline Field", "They can also be inline.", true)
   /*
    * Blank field, useful to create some space.
    */
   .addBlankField(true)
-  .addField("", "", true);
-
-  //msg.channel.send({embed});
-	  
-msg.guild.channels.find("name", "raids-pinhal-novo").sendMessage({embed});	  
+  .addField("Inline Field 3", "You can have a maximum of 25 fields.", true);  
+   msg.guild.channels.find("name", "raids-pinhal-novo").sendMessage({embed});	  
 	  
 	  
 	  

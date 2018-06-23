d127854dd04764fd1e7cbc25bf1c8a7b86465f12
ist188123 <paulorosario@sapo.pt>
2018-06-23 12:26:58 +0100

    Update index.js

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ if (msg.channel.name == 'raids-marcacao') {
    * Alternatively, use "#00AE86", [0, 174, 134] or an integer number.
    */
   .setColor(0x00AE86)
-  .setDescription("Para se inscrever utilize o canal: "+refcanal.concat("#",nomecanal) )
+  .setDescription("Para #raid5-piscinas-14h30 se inscrever utilize o canal: "+refcanal.concat("#",nomecanal) )
   .setFooter("PG pinhal novo, pubicado :", "https://exraidspinhalnovo.webnode.pt/_files/200000022-231042409e/200/damasc010.png")
  // .setImage("http://i.imgur.com/yVpymuV.png")
   .setThumbnail("https://exraidspinhalnovo.webnode.pt/_files/200000018-6874a696da/450/regice.png")

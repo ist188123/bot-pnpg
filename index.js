613c7dd1dfbdc478238c9a0c3c3afc9ac8f3d833
ist188123 <paulorosario@sapo.pt>
2018-06-23 20:39:36 +0100

    Update index.js

--- index.js
+++ index.js
@@ -64,7 +64,7 @@ if (msg.channel.name == 'raids-marcacao') {
 		  if(msg.guild.channels.find("name", nomecanal  )){
 		  }else{
 			  
-	msg.guild.createChannel(canal, "text");
+	msg.guild.createChannel(canal, "text", {parent: "RAIDS-ATIVAS"});
 	//msg.channel.setParent('355908108431917066', { lockPermissions: false })
 		  }	  
 		  

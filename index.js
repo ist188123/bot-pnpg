9e7d02ba1c9d8b2ff5e2c0e6d5e8acf2b62eae46
ist188123 <paulorosario@sapo.pt>
2018-06-24 21:06:15 +0100

    Update index.js

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ client.on('message', msg => {
 
 function criaRaid(ncanal,text,treinador){
   //inicio mensagem
-	var totaltreinadores=treinador.trim().split("@")
+	var totaltreinadores=treinador.split("@").trim()
 	//"!raid5 piscina !12h30".substring(1)
 			var titulo="RAID 5 "+ncanal.substr(7)
 			var thoras=titulo.split("-")
@@ -61,7 +61,7 @@ for (var i=0;i<y.length-1;i++){
   /*
    * lista dos jogadores que vão RAID.
    */
-  .addField("Treinadores:["+totaltreinadores.length+"]",treinador , true)
+  .addField("Treinadores:["+totaltreinadores.length+"]",totaltreinadores , true)
   /*
    * Blank field, useful to create some space.
    */

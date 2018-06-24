72e8db4bd3b12ca5144e8779429acf33b2a9b48a
ist188123 <paulorosario@sapo.pt>
2018-06-24 10:30:48 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
  const config = require("./config.json");
-
+const sql = require("sqlite");
+sql.open("./score.sqlite");
 
 
 client.on('ready', () => {

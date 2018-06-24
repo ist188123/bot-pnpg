f4b016fdac09908bd268197a04657b4db2449d46
ist188123 <paulorosario@sapo.pt>
2018-06-24 21:43:19 +0100

    Update index.js

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ for (var i=0;i<y.length-1;i++){
    * Alternatively, use "#00AE86", [0, 174, 134] or an integer number.
    */
   .setColor(0x00AE86)
-  .setDescription("Registo utiliza o canal: "+ msg.guild.channels.get("name",ncanal))
+  .setDescription("Registo utiliza o canal: "+ msg.channel.name)
   .setFooter("PG pinhal novo, pubicado :", "https://exraidspinhalnovo.webnode.pt/_files/200000022-231042409e/200/damasc010.png")
  // .setImage("http://i.imgur.com/yVpymuV.png")
   .setThumbnail("https://exraidspinhalnovo.webnode.pt/_files/200000018-6874a696da/450/regice.png")

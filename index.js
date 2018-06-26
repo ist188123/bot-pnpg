b8fda5c00567d4b07f2abdb5420a893809a17342
ist188123 <paulorosario@sapo.pt>
2018-06-26 02:27:33 +0100

    Update index.js

--- index.js
+++ index.js
@@ -87,12 +87,56 @@ for (var i=0;i<y.length-1;i++){
 
 
 // fim funcao
+function info(){
+	msg.guild.channels.find("name", "informacao").sendMessage({embed: {
+	
+    color: 3447003,
+    author: {
+      name: client.user.username,
+      icon_url: client.user.avatarURL
+    },
+    title: "This is an embed",
+    url: "http://google.com",
+    description: "This is a test embed to showcase what they look like and what they can do.",
+    fields: [{
+        name: "Fields",
+        value: "They can have different fields with small headlines."
+      },
+      {
+        name: "Masked links",
+        value: "You can put [masked links](http://google.com) inside of rich embeds."
+      },
+      {
+        name: "Markdown",
+        value: "You can put all the *usual* **__Markdown__** inside of them."
+      }
+    ],
+    timestamp: new Date(),
+    footer: {
+      icon_url: client.user.avatarURL,
+      text: "© Example"
+    }
+  }
+});
+	
+	
+	
+	
+	
+	
+}
 
 
 
 
-
-
+if (msg.channel.name == 'adm-pg-pinhalnovo') {
+	
+	 if(msg.content.startsWith("!")){
+	info()
+	
+	 }
+	
+}
 
 
 

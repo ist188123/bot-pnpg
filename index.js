7a7eb1127f5be258b54dc706c5c1327c7fb845da
ist188123 <paulorosario@sapo.pt>
2018-06-29 15:25:02 +0100

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,18 @@ bot.on('ready', () => {
   bot.on('message', message => {
     
     
-    
+    if (message.content === 'spec'){
+        message.author.send("See or Change?");
+        const collector = new Discord.MessageCollector(message.channel, m => m.author.id === message.author.id, { time: 10000 });
+        console.log(collector)
+        collector.on('collect', message => {
+            if (message.content == "See") {
+                message.channel.send("You Want To See Someones Spec OK!");
+            } else if (message.content == "Change") {
+                message.channel.send("You Want To Change Your Spec OK!");
+            }
+        }
+    }
     
     
     

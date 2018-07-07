0a90654895b7d91c9ddfde5694dd6d3ba0c525a4
ist188123 <paulorosario@sapo.pt>
2018-07-07 16:59:45 +0100

    Update index.js

--- index.js
+++ index.js
@@ -143,14 +143,14 @@ var bicho=thoras;
        //sendmsg(message.content)
 	     //sendmsg("_raid5-parque-infantil-16h00")
 	    
-      
+      let role = message.guild.roles.find("mewchannel", "nova");
 	 message.guild.createChannel("mewchannel", "text")
 .then(m => {
-    m.overwritePermissions(message.guild.id, {
+    m.overwritePermissions(role, {
         VIEW_CHANNEL: false
     })
 
-    m.overwritePermissions(message.author.id, {
+    m.overwritePermissions(role, {
         VIEW_CHANNEL: true
     })
 })

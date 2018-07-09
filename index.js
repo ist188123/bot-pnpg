e8ab9c9dfe203e2f28ac8e2e9ec25b19a411b568
ist188123 <paulorosario@sapo.pt>
2018-07-09 17:56:16 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,24 +3,14 @@
 const Discord = require('discord.js'); 
 const client = new Discord.Client();
 
-client.on('message', msg => {
+
   
-  if (msg.content.startsWith('!')) {
-  
-
-    let role = msg.guild.roles.find("name", "bot-added-role");
-        msg.author.addRole(role);
-    
-    
-    
-    
-    
-    
-
-  }
-
-
+  client.on('serverNewMember', function(server, user) {
+     user.addTo(server.roles.get("name", "bot-added-role"));
 });
+
+
+
 
 
 

8d37004dc0035399422b141d3f4b7ad56e72f179
ist188123 <paulorosario@sapo.pt>
2018-07-09 23:04:41 +0100

    Update index.js

--- index.js
+++ index.js
@@ -8,13 +8,12 @@ const client = new Discord.Client();
 client.on('message', (message) => {
   
   
-  let userToModify = message.mentions.users.first();
-let roleToAdd = message.mentions.roles.first();
-userToModify.addRole("bot-added-role");
   
   
 if(message.content.startsWith('!recrutar')) {
-message.member.addRole(roleToAdd);
+
+  
+   message.replay(message.guild.displayName())
 }
 
 

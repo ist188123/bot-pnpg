84389b39ad23625d1672ba350fd7405e6af378bf
ist188123 <paulorosario@sapo.pt>
2018-07-11 22:59:30 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,166 +1,38 @@
 
 
-const Discord = require('discord.js');
+const Discord = require("discord.js");
+const fs = require("fs");
 const client = new Discord.Client();
- 
 
+let points = JSON.parse(fs.readFileSync("./points.json", "utf8"));
+const prefix = "+";
 
+client.on("message", message => {
+  if (!message.content.startsWith(prefix)) return;
+  if (message.author.bot) return;
 
+  if (!points[message.author.id]) points[message.author.id] = {
+    points: 0,
+    level: 0
+  };
+  let userData = points[message.author.id];
+  userData.points++;
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+  let curLevel = Math.floor(0.1 * Math.sqrt(userData.points));
+  if (curLevel > userData.level) {
+    // Level up!
+    userData.level = curLevel;
+    message.reply(`You"ve leveled up to level **${curLevel}**! Ain"t that dandy?`);
+  }
+
+  if (message.content.startsWith(prefix + "level")) {
+    message.reply(`You are currently level ${userData.level}, with ${userData.points} points.`);
+  }
+  fs.writeFile("./points.json", JSON.stringify(points), (err) => {
+    if (err) console.error(err)
+  });
+
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-client.on('message', msg => {
-  
- 
- 
- 
- 
- 
-
-	
-function ev(vai){
-	
-	
-	var local="";
-	var divide=vai.split(" ");
-	var metecor="";
-	
-	     var member= "";
-for (var i=0 ; i< divide.length-1 ; i++){
-	metecor="";
-	
-	//let member = msg.mentions.members.first();
-	//if(member.roles.some(r=>["Dev", "Mod", "Server Staff", "Proficient"].includes(r.name)) ) {
-	//metecor="ccccc";
-	//}
-	
-	
-    
-	
-	
-	
-	
-   local=local+"\n\:valor: "+divide[i];
-   
-}
-		
- 
- 
- 
- const embed = new Discord.RichEmbed()
-  .setTitle("Utilize o canal para registo ")
-  .setAuthor(msg.guild.channels.find("name", msg.channel.name), "http://i.imgur.com/w1vhFSR.png")
-  /*
-   * Alternatively, use "#00AE86", [0, 174, 134] or an integer number.
-   */
-  .setColor(0x00AE86)
-  .setDescription("This is the main body of text, it can hold 2048 characters.")
-  .setFooter("This is the footer text, it can hold 2048 characters", "http://i.imgur.com/w1vhFSR.png")
-  .setImage("http://i.imgur.com/yVpymuV.png")
-  .setThumbnail("http://i.imgur.com/p2qNFag.png")
-  /*
-   * Takes a Date object, defaults to current date.
-   */
-  .setTimestamp()
-  .setURL("https://discord.js.org/#/docs/main/indev/class/RichEmbed")
-  .addField("This is a field title, it can hold 256 characters",local )
-  /*
-   * Inline fields may not display as inline if the thumbnail and/or image is too big.
-   */
-  .addField("Inline Field", "They can also be inline.", true)
-  /*
-   * Blank field, useful to create some space.
-   */
-  .addBlankField(true)
-  .addField("Inline Field 3", "You can have a maximum of 25 fields.", true);
-
-  msg.channel.send({embed});
- 
- 
- 
- 
-}
-
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 		   
-		  var obj = {
-    length: 0,
-
-    addElem: function addElem(elem) {
-        // obj.length is automatically incremented 
-        // every time an element is added.
-        [].push.call(this, elem);
-    }
-};
- 
- 
- 
- if (msg.content.startsWith('!')) {
-
-  var coisa="\:poop: @jcpjorge @Damasc010 <:valor:460526619124039691>@sira SDamac022<:valor:460526619124039691> @Damasc010 <:valor:460526619124039691>"
-  
-  
-  
-// Check if they have one of many roles
-if(msg.member.roles.some(r=>["Dev", "Mod", "Server Staff", "Proficient"].includes(r.name)) ) {
-
- 
- ev("@manuel @Damasc010 @sira SDamac022 @Damasc010");
-} else {
-  // has none of the roles
- msg.reply("no");
-}
-  
-  
- }
- 
- 
- 
- 
- 
-});
-
-
-
-
 
 
 client.login(process.env.BOT_TOKEN);

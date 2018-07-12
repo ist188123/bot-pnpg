94fb9485016a32bfe09f6aab79dd4a18b9117495
ist188123 <paulorosario@sapo.pt>
2018-07-12 11:41:14 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 
 const client = new Discord.Client();
 
-let points = JSON.parse(fs.readFileSync("./points.json", "utf8"));
+
 const prefix = "+";
 
 client.on("message", msg => {

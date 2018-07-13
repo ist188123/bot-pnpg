77337ec810a150978972d1753d2f530094904aa3
ist188123 <paulorosario@sapo.pt>
2018-07-13 15:46:57 +0100

    Update index.js

--- index.js
+++ index.js
@@ -41,25 +41,6 @@ member.addRole(role);
     
     //.----
   
-  if(msg.content.startsWith("//Mod")){
-    let roleName = msg.content.split(" ").slice(1).join(" ");
-
-    //Filtering the guild members only keeping those with the role
-    //Then mapping the filtered array to their usernames
-    let membersWithRole = msg.guild.members.filter(member => { 
-        return member.roles.find("name", roleName);
-    }).map(member => {
-        return member.user.username;
-    })
-
-    let embed = new discord.RichEmbed({
-        "title": `Users with the ${roleName} role`,
-        "description": membersWithRole.join("\n"),
-        "color": 0xFFFF
-    });
-
-    return msg.channel.send({embed});
-}
   
   
   //-----

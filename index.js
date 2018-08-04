ce5bb1f88b482c068db5b029ccb169fb9637798a
ist188123 <paulorosario@sapo.pt>
2018-08-04 13:36:36 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,13 +6,6 @@ const client = new Discord.Client();
 const prefix = "+";
 
 
-client.on('messageReactionAdd', (reaction, user) => {
-    if(reaction.emoji.name === "✅") {
-        console.log(reaction.users);
-    }
-});
-
-
 
 client.on("message", msg => {
   

1a97944746cff73cdccbb6d6679c7b8492865f0d
ist188123 <paulorosario@sapo.pt>
2018-08-07 12:54:50 +0100

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ if (msg.content.startsWith('👍')) {
     msg.reply(msg.author);
    msg.guild.members.get(msg.author.id).addRole(role);
      
-    msg.reply(msg.guild.channel.name);
+    msg.reply(msg.guild.channel);
   }     
   
   if (msg.content.startsWith('👎')) {

7a8ea37071bcb2318cbe786777f8fa413befbaae
ist188123 <paulorosario@sapo.pt>
2018-08-07 17:29:11 +0100

    Update index.js

--- index.js
+++ index.js
@@ -92,33 +92,34 @@ client.on("message", msg => {
 function criaRaid(canal,treinadores,total,tier){
   
   const embed = new Discord.RichEmbed()
-  .setTitle("This is your title, it can hold 256 characters")
-  .setAuthor("Author Name", "https://i.imgur.com/lm8s41J.png")
+  .setTitle("17h00")
+  .setAuthor("Author Name", ovo)
   /*
    * Alternatively, use "#00AE86", [0, 174, 134] or an integer number.
    */
   .setColor(0x00AE86)
-  .setDescription("This is the main body of text, it can hold 2048 characters.")
-  .setFooter("This is the footer text, it can hold 2048 characters", "http://i.imgur.com/w1vhFSR.png")
-  .setImage("http://i.imgur.com/yVpymuV.png")
+  .setDescription("Registo : "+msg.guild.channels.find("name", canal))
+  
+ 
+  .setFooter("Equipa: PKG Pinhal Novo, pubicado :", "https://exraidspinhalnovo.webnode.pt/_files/200000022-231042409e/200/damasc010.png")
+  //.setImage("http://i.imgur.com/yVpymuV.png")
   .setThumbnail("http://i.imgur.com/p2qNFag.png")
   /*
    * Takes a Date object, defaults to current date.
    */
   .setTimestamp()
   .setURL("https://discord.js.org/#/docs/main/indev/class/RichEmbed")
-  .addField("This is a field title, it can hold 256 characters",
-    "This is a field value, it can hold 2048 characters.")
+  
+  .addField("Niveis CP", " ")
   /*
    * Inline fields may not display as inline if the thumbnail and/or image is too big.
    */
-  .addField("Inline Field", "They can also be inline.", true)
+  addField("Treinadores : "+total, treinadores, true)
   /*
    * Blank field, useful to create some space.
    */
   .addBlankField(true)
-  .addField("Inline Field 3", "You can have a maximum of 25 fields.", true);
-
+  .addField("Fraco contra:","", true);  
   
   
   msg.guild.channels.find("name", "raids-pinhal-novo").sendMessage({embed});

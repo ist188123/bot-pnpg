7b57a716551205645b53d5d5de2d3b24b94af343
ist188123 <paulorosario@sapo.pt>
2018-08-09 12:30:51 +0100

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ client.on("message", msg => {
    let obs="";
    var regra="Mod"
   let pkmraid="";
-	let adicional=" ";
+	let adicional="Treinadores:\n";
   
  //  let role = msg.guild.roles.find("name", regra);
   
@@ -337,7 +337,7 @@ function disparaRaid(local,horas,canal,treinadores,total,ovo,bicho,cpiv,status,a
   
   
   .addBlankField(true)
-  .addField("Observações:", "\n"+adicional, true)
+  .addField("Observações:", adicional, true)
   
   
   .addBlankField(true)

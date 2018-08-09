7c81a5a9db17579dc4ad7a6142333b366c066415
ist188123 <paulorosario@sapo.pt>
2018-08-09 11:28:26 +0100

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ client.on("message", msg => {
    let obs="";
    var regra="Mod"
   let pkmraid="";
-	let adicional="";
+	let adicional="+Não";
   
  //  let role = msg.guild.roles.find("name", regra);
   

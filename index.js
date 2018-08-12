34c42850183497d201c70bf464faa1b2e49e8579
ist188123 <paulorosario@sapo.pt>
2018-08-12 14:20:14 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 
 const client = new Discord.Client();
-
+  const fs = require('fs');
 const prefix = "+";
 
 
@@ -42,10 +42,10 @@ client.on("message", async (msg) => {
 
 
         //apaga mensagem - retira da raid
-        if (msg.content.startsWith('!oi')) {
+        if (msg.content.startsWith('%')) {
             
 
-            const fs = require('fs');
+          
 
 fs.readFile('student.json', (err, data) => {  
     if (err) throw err;

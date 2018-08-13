709908b1700933043633a9b96806831d199b22cd
ist188123 <paulorosario@sapo.pt>
2018-08-13 20:06:06 +0100

    Update index.js

--- index.js
+++ index.js
@@ -39,11 +39,6 @@ client.on("message", async (msg) => {
         "regirock": "**REGIROCK**\n",
         
         
-        
-        
-        
-        
-        
         "machamp" : "RAID BOSS CP 18144\nCaptura 100% IV : 1574-1967 1650-2063\n"+
                     "**Fraco contra:**\n<:psiquico:478587230483513346><:voador:478587231259590676><:fada:478587230814863372>\n"+
         "Alakazam: Futuresight(33.00)\nEspeon: Futuresight(35.60)\nExeggutor: Confusion(30.10)\nCharizard: Overheat(32.20)\nDragonite: Dragon Hurricane(33.95)"

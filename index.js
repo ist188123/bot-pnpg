390aa3ba08ee62153d1222d86558597b349ca062
ist188123 <paulorosario@sapo.pt>
2018-08-14 12:40:05 +0100

    Update index.js

--- index.js
+++ index.js
@@ -398,7 +398,7 @@ let cor="0x00AE86";
       if (sponser == "exraid") {
         cor="0XFEAFEA";
         ovo="https://exraidspinhalnovo.webnode.pt/_files/200000023-29ab72ab0f/450/Mewtwo.png"
-       // horas = horas + "\nGinásio em pontuação para o Mewtwo";
+        horas = horas + "\nGinásio em pontuação para o Mewtwo";
       }
       //----  FIM TIPO RAID ---
 

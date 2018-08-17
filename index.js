86684ca7f288d62887cae00a7db760160321fffc
ist188123 <paulorosario@sapo.pt>
2018-08-17 19:24:27 +0100

    Update index.js

--- index.js
+++ index.js
@@ -15,11 +15,13 @@ client.on("message", async (msg) => {
     
 if (msg.content.startsWith('tempo')) {
     
-        
-let apiKey = process.env.WHEATHER;
-let city = "portland";
-let url = "http://api.openweathermap.org/data/2.5/weather?q=pinhal%20novo,pt&appid=e5b13f9f228fb556488b01e0cb51be00"
 
+const request = require('request');
+request('http://api.openweathermap.org/data/2.5/weather?q=pinhal%20novo,pt&appid=e5b13f9f228fb556488b01e0cb51be00', { json: true }, (err, res, body) => {
+if (err) { return msg.reply(err); }
+msg.reply(body);
+
+});
    
 
 

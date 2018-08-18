ff5ad0ff079329cf8147d3f99331c01767da390e
ist188123 <paulorosario@sapo.pt>
2018-08-18 12:55:20 +0100

    Update index.js

--- index.js
+++ index.js
@@ -9,6 +9,23 @@ const prefix = "+";
 
 
 client.on("message", async (msg) => {
+    
+    
+    
+     if (msg.content.startsWith('xpto')) {
+    
+    const request = require('request');
+request('http://api.openweathermap.org/data/2.5/weather?q=pinhal%20novo,pt&appid=e5b13f9f228fb556488b01e0cb51be00', { json: true }, (err, res, body) => {
+if (err) { return console.log(err); }
+console.log(body);
+
+});
+    
+     } 
+    
+    
+    
+    
     let mewtwo = "";
     let cor = "0x00AE86";
     let mais = "";

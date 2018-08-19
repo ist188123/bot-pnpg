e8c1854594a83fcddcc47653398a2e134cfbc3c1
ist188123 <paulorosario@sapo.pt>
2018-08-19 19:34:35 +0100

    Update index.js

--- index.js
+++ index.js
@@ -76,9 +76,45 @@ return textTime = new Date(sunriseMills+(3600000*offset))
 
 
     function myFunc(arg) {
-        var textoMensagem="Codigos que podes utilizar:\nAdicionar pokemon, exemplo: %regirock\nGinásio em pontuação, exemplo: !exraid\nPara mais informação consultar "+msg.guild.channels.find("name", "willow-tutorial");
+      //  var textoMensagem="Codigos que podes utilizar:\nAdicionar pokemon, exemplo: %regirock\nGinásio em pontuação, exemplo: !exraid\nPara mais informação consultar "+msg.guild.channels.find("name", "willow-tutorial");
         
-        msg.guild.channels.find("name", arg).sendMessage("Olá Treinadores,\n"+textoMensagem+"\nEste canal é temporário e será apagado às : "+horaCanal('+2.5'));
+      //  msg.guild.channels.find("name", arg).sendMessage("Olá Treinadores,\n"+textoMensagem+"\nEste canal é temporário e será apagado às : "+horaCanal('+2.5'));
+   
+    
+    msg.guild.channels.find("name", arg).sendMessage({embed: {
+    color: 3447003,
+    author: {
+      name: client.user.username,
+      icon_url: client.user.avatarURL
+    },
+    title: "This is an embed",
+    url: "http://google.com",
+    description: "This is a test embed to showcase what they look like and what they can do.",
+    fields: [{
+        name: "Fields",
+        value: "They can have different fields with small headlines."
+      },
+      {
+        name: "Masked links",
+        value: "You can put [masked links](http://google.com) inside of rich embeds."
+      },
+      {
+        name: "Markdown",
+        value: "You can put all the *usual* **__Markdown__** inside of them."
+      }
+    ],
+    timestamp: new Date(),
+    footer: {
+      icon_url: client.user.avatarURL,
+      text: "© Example"
+    }
+  }
+});
+    
+    
+    
+    
+    
     }
 
 

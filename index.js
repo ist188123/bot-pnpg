407260edab93ea3969a3f56deaa49d25dad8c906
ist188123 <paulorosario@sapo.pt>
2018-08-23 22:27:14 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,13 +6,13 @@ const prefix = "+";
 
 var url = "http://api.openweathermap.org/data/2.5/weather?q=pinhal novo,uk&appid=e5b13f9f228fb556488b01e0cb51be00"
 
-var request = require("request")
+
 
  client.on("message", async (msg) => {
      
      if (msg.channel.name.startsWith('!xpto')) {
 
-var jsonData = require('./users.json');
+var jsonData = require('./pkpn.json');
 msg.reply(jsonData.RAIDS);
      }
      

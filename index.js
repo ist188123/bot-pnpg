1e07638553250b9402f2fd379398d269d783302c
ist188123 <paulorosario@sapo.pt>
2018-08-23 22:44:59 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,17 +2,14 @@ const Discord = require("discord.js");
 
 const client = new Discord.Client();
 
-const prefix = "+";
-
-var url = "http://api.openweathermap.org/data/2.5/weather?q=pinhal novo,uk&appid=e5b13f9f228fb556488b01e0cb51be00"
-
+var jsonData = require('./raidspn.json');
 
 
  client.on("message", async (msg) => {
      
      if (msg.channel.name.startsWith('!xpto')) {
 
-var jsonData = require('./raidspn.json');
+
 msg.reply(jsonData.RAIDS);
      }
      

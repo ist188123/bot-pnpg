f2f6b7e2a32972b8bb4ff5756e75101427c0e1b3
ist188123 <paulorosario@sapo.pt>
2018-08-23 23:32:53 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 
 const client = new Discord.Client();
 
-var jsonData = require('./raidspn.json');
+var jsonData = require('raidspn.json');
 
 
  client.on("message", async (msg) => {

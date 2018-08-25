fed935ea01d7febac4d82f2c2f229aa96e472b35
ist188123 <paulorosario@sapo.pt>
2018-08-25 18:40:11 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,42 +2,20 @@ const Discord = require("discord.js");
 
 const client = new Discord.Client();
 
-var jsonData = require('./raidspn.json');
-var fs = require('fs')
+const prefix = "+";
 
- client.on("message", async (msg) => {
-     
-     if (msg.content.startsWith('!xpto')) {
 
-//msg.reply("coisa");/
-      msg.reply(jsonData.RAIDS._raid4.tyra.cpboss);
-      
+
+
+
+client.on("message", async (msg) => {
     
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-     }
-     
-     
-     
+        if (msg.content.startsWith('!')) {
+msg.reply("oi");
+           
+        }
+   
+
 });
-        
 
 client.login(process.env.BOT_TOKEN);

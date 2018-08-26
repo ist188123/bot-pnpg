5628f22e5378a780710953df3ef9d228ca7dc11a
ist188123 <paulorosario@sapo.pt>
2018-08-26 12:56:32 +0100

    Update index.js

--- index.js
+++ index.js
@@ -39,8 +39,8 @@ client.on("message", async (msg) => {
     if (resposta[msg.content]) {
         msg.channel.send({embed: {
   color: 16580627,
-       addField :"BOSS CP ",
-  description: "Resposta a : "+msg.content+"\n"+resposta[msg.content],
+      
+  description: "**Resposta a : **"+msg.content+"\n\n\n"+resposta[msg.content],
             timestamp: new Date(),
                 footer: {
                     icon_url: "https://exraidspinhalnovo.webnode.pt/_files/200000022-231042409e/200/damasc010.png",
